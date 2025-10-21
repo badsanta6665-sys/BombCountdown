a2d573b2cccc289abeb402959102bb42b324e77a
badsanta6665-sys <badsanta6665@googlemail.com>
2025-10-21 09:27:33 +0200

    Add initial Plugin.cs project file

---
+++ Plugin.cs
@@ -0,0 +1,12 @@
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <Nullable>enable</Nullable>
+    <OutputType>Library</OutputType>
+    <CopyLocalLockFileAssemblies>true</CopyLocalLockFileAssemblies>
+  </PropertyGroup>
+  <ItemGroup>
+    <PackageReference Include="CounterStrikeSharp.API" Version="1.0.295" />
+  </ItemGroup>
+</Project>
